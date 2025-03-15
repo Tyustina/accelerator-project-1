efd4528e783e7d9afae329f2e07d66c21245af33
Anastasiia Tyustina <103989119+Tyustina@users.noreply.github.com>
2025-03-15 18:43:17 +0500

    Добавляет раздел FAQ и код JS для его анимации

--- source/js/faq.js
+++ source/js/faq.js
@@ -1,24 +1,54 @@
-const faqList = document.querySelector('.faq__list');
-const faqItems = faqList.querySelectorAll('.faq__item');
+const faqSection = document.querySelector('.faq');
+const faqLists = faqSection.querySelectorAll('.faq__list');
 
-faqItems.forEach(faqItem => {
-  const faqAnswer = faqItem.querySelector('p');
-  const faqBtn = faqItem.querySelector('.faq__button');
-  faqAnswer.style.maxHeight = '0';
-  faqAnswer.style.overflow = 'hidden';
-  faqAnswer.style.transition = 'max-height 0.3s ease-out';
+faqLists.forEach(faqList => {
+  const faqItems = faqList.querySelectorAll('.faq__item');
 
-  faqBtn.addEventListener('click', ()=>{
-    if(faqBtn.classList.contains('faq__button--closed')) {
-      faqBtn.classList.remove('faq__button--closed');
-      faqBtn.classList.add('faq__button--opened');
-      faqAnswer.style.maxHeight = faqAnswer.scrollHeight + 'px';
-    }else{
-      faqBtn.classList.remove('faq__button--opened');
-      faqBtn.classList.add('faq__button--closed');
-      faqAnswer.style.maxHeight = '0';
-    }
-  })
+  faqItems.forEach(faqItem => {
+    const faqAnswer = faqItem.querySelector('p');
+    const faqBtn = faqItem.querySelector('.faq__button');
+
+    faqAnswer.style.maxHeight = '0';
+    faqAnswer.style.overflow = 'hidden';
+    faqAnswer.style.transition = 'max-height 0.3s ease-out';
+
+    faqBtn.addEventListener('click', () => {
+      if (faqBtn.classList.contains('faq__button--closed')) {
+        faqBtn.classList.remove('faq__button--closed');
+        faqBtn.classList.add('faq__button--opened');
+        faqAnswer.style.maxHeight = faqAnswer.scrollHeight + 'px';
+      } else {
+        faqBtn.classList.remove('faq__button--opened');
+        faqBtn.classList.add('faq__button--closed');
+        faqAnswer.style.maxHeight = '0';
+      }
+    });
+  });
+});
+
+const showFaq = (btn) => {
+
+  const targetId = btn.dataset.targetId;
+  const elFaqList = faqSection.querySelector(`.faq__list[data-id="${targetId}"]`);
+
+  if (elFaqList) {
+    const tabActive = faqSection.querySelector('.faq__tab--active');
+    tabActive.classList.remove('faq__tab--active');
+    const faqListActive = faqSection.querySelector('.faq__list--active');
+    faqListActive.classList.remove('faq__list--active');
+    btn.classList.add('faq__tab--active');
+    elFaqList.classList.add('faq__list--active');
+  }
+}
+
+faqSection.addEventListener('click', (e) => {
+  if (!e.target.closest('.faq__tabs')) {
+    return;
+  }
+  const btn = e.target.closest('.faq__tab');
+  if (btn) {
+    showFaq(btn);
+  }
 })
 
 
