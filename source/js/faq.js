6f1261c77e8b581e4e1942a0cac9312f35a45b13
Anastasiia Tyustina <103989119+Tyustina@users.noreply.github.com>
2025-03-18 00:39:16 +0500

    Добавляет корректировки по PP

--- source/js/faq.js
+++ source/js/faq.js
@@ -1,30 +1,37 @@
 const faqSection = document.querySelector('.faq');
 const faqLists = faqSection.querySelectorAll('.faq__list');
 
-faqLists.forEach(faqList => {
-  const faqItems = faqList.querySelectorAll('.faq__item');
+// faqLists.forEach(faqList => {
+//   const faqItems = faqList.querySelectorAll('.faq__item');
 
-  faqItems.forEach(faqItem => {
-    const faqAnswer = faqItem.querySelector('p');
-    const faqBtn = faqItem.querySelector('.faq__button');
+//   faqItems.forEach((faqItem, index) => {
+//     const faqAnswer = faqItem.querySelector('p');
+//     const faqBtn = faqItem.querySelector('.faq__button');
 
-    faqAnswer.style.maxHeight = '0';
-    faqAnswer.style.overflow = 'hidden';
-    faqAnswer.style.transition = 'max-height 0.3s ease-out';
+//     // faqAnswer.style.maxHeight = '0';
+//     // faqAnswer.style.overflow = 'hidden';
+//     // faqAnswer.style.transition = 'max-height 0.3s ease-out';
 
-    faqBtn.addEventListener('click', () => {
-      if (faqBtn.classList.contains('faq__button--closed')) {
-        faqBtn.classList.remove('faq__button--closed');
-        faqBtn.classList.add('faq__button--opened');
-        faqAnswer.style.maxHeight = faqAnswer.scrollHeight + 'px';
-      } else {
-        faqBtn.classList.remove('faq__button--opened');
-        faqBtn.classList.add('faq__button--closed');
-        faqAnswer.style.maxHeight = '0';
-      }
-    });
-  });
-});
+//     // if (index === 0 ) {
+//     //   faqBtn.classList.remove('faq__button--closed');
+//     //   faqBtn.classList.add('faq__button--opened');
+//     //   faqAnswer.style.maxHeight = faqAnswer.scrollHeight + 'px';
+//     // }
+
+
+//     faqBtn.addEventListener('click', () => {
+//       if (faqBtn.classList.contains('faq__button--closed')) {
+//         faqBtn.classList.remove('faq__button--closed');
+//         faqBtn.classList.add('faq__button--opened');
+//         faqAnswer.style.maxHeight = faqAnswer.scrollHeight + 'px';
+//       } else {
+//         faqBtn.classList.remove('faq__button--opened');
+//         faqBtn.classList.add('faq__button--closed');
+//         faqAnswer.style.maxHeight = '0';
+//       }
+//     });
+//   });
+// });
 
 const showFaq = (btn) => {
 
