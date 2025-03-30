542f010d9cd55a7376d8e7dc162778e5538e5e62
Anastasiia Tyustina <103989119+Tyustina@users.noreply.github.com>
2025-03-30 15:06:35 +0500

    Добавляет изменения по тз

--- source/js/video.js
+++ source/js/video.js
@@ -3,14 +3,30 @@ const videoButton = videoContainer.querySelector('.video__button');
 const videoLink = videoContainer.querySelector('.video__link');
 const videoPlaceholder = videoContainer.querySelector('.video__placeholder');
 
-const videoURL = 'https://www.youtube.com/embed/9TZXsZItgdw?si=-0rn2RyAriNlx1fp?autoplay=1';// Замените на URL вашего видео
+const videoURL = 'https://www.youtube.com/embed/9TZXsZItgdw?si=j-uixdShC64SU4wE&autoplay=1';
 
-videoButton.addEventListener('click', () => {
-  // Создаем iframe с нужными атрибутами
-  videoLink.src = videoURL;
-  videoLink.setAttribute('allow', 'autoplay'); // Needed for some browsers to autoplay.
-  // Скрываем placeholder и кнопку
+function playVideo () {
+  // videoLink.src = videoURL;
+  videoLink.innerHTML = `
+  <iframe width="100%" height="100%"
+    src="https://www.youtube.com/embed/9TZXsZItgdw?si=j-uixdShC64SU4wE&autoplay=1"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer;
+    autoplay;
+    clipboard-write;
+    encrypted-media;
+    gyroscope;
+    picture-in-picture;
+    web-share"
+    referrerpolicy="strict-origin-when-cross-origin"
+    allowfullscreen>
+  </iframe>
+`;
+
   videoPlaceholder.style.display = 'none';
   videoButton.style.display = 'none';
   videoContainer.classList.add('video--no-after');
-});
+}
+
+videoButton.addEventListener('click', playVideo);
