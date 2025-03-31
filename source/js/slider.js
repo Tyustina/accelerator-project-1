3c4c3b76233cc3d00abc8cd327da4404396fa19d
Anastasiia Tyustina <103989119+Tyustina@users.noreply.github.com>
2025-03-31 15:20:16 +0500

    Исправляет ошибки

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,32 +1,29 @@
 import Swiper from 'swiper';
-import {Navigation, Mousewheel} from 'swiper/modules';
+import { Navigation, Mousewheel } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/mousewheel';
 
 const trainersSwiper = new Swiper('.juri__swiper', {
-  modules: [Navigation, Mousewheel ],
+  modules: [Navigation, Mousewheel],
   loop: true,
   navigation: {
     nextEl: '.juri__button--next',
     prevEl: '.juri__button--prev',
   },
-  allowTouchMove: true,
   spaceBetween: 40,
   slidesPerView: 1,
-  speed: 400,
+  slidesPerGroup: 1,
   breakpoints: {
-    320: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
-    },
     768: {
       slidesPerView: 2,
-      slidesPerGroup: 2,
     },
     1366: {
       slidesPerView: 4,
-      slidesPerGroup: 4,
+      touch: false,
+      simulateTouch: false,
+      mousewheel: false,
+      keyboard: false,
     }
   }
 });
@@ -38,22 +35,19 @@ const reviewsSwiper = new Swiper('.reviews__swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  allowTouchMove: true,
-  spaceBetween: 10,
+  spaceBetween: 40,
   slidesPerView: 1,
   slidesPerGroup: 1,
+  touch: true,
+  simulateTouch: true,
+  mousewheel: true,
+  keyboard: true,
   breakpoints: {
-    320: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
-    },
-    768: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
-    },
     1366: {
-      slidesPerView: 1,
-      slidesPerGroup: 1,
+      touch: false,
+      simulateTouch: false,
+      mousewheel: false,
+      keyboard: false,
     }
   }
 });
